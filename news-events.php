2ced97e4e17980ecd7cc6c2aaa471c01ad8fb105
jeffdnganga <jeffdaniels950@gmail.com>
2021-08-09 10:45:51 +0300

    Banner for News and Events page

--- news-events.php
+++ news-events.php
@@ -6,7 +6,15 @@ include('includes/navigation.php');
 
 ?>
 
-
+<section class="news_events_banner">
+    <div class="container-fluid">
+        <div class="news_banner_msg">
+            <center>
+                <p class="h1 text-white">News and Events</p>
+            </center>
+        </div>
+    </div>
+</section>
 
 
 <?php
